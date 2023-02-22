2e55e3955f5a5d0553e9d6950523d894b8a33599
mrbinh008 <binhvtx3@gmail.com>
2023-02-22 20:27:53 +0700

    fix 2

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
 // File path: ./app.js
 
 // Config các biến môi trường
-require('dotenv').config();
-const TelegramBot = require('node-telegram-bot-api');
-const ChatGPTService = require('./services/chatgpt.service');
+require("dotenv").config();
+const TelegramBot = require("node-telegram-bot-api");
+const ChatGPTService = require("./services/chatgpt.service");
 
 const telegramToken = process.env.TELEGRAM_KEY;
 
 // Khởi tạo con Bot từ Token với chế độ Polling
-const bot = new TelegramBot(telegramToken, {polling: true});
+const bot = new TelegramBot(telegramToken, { polling: true });
 
-bot.on('message', (msg) => {
-  const chatId = msg.chat.id;       // ID của cuộc trò chuyện hiện tại
-  const chatMsg = msg.text;         // Nội dung của tin nhắn đã nhận
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id; // ID của cuộc trò chuyện hiện tại
+  const chatMsg = msg.text; // Nội dung của tin nhắn đã nhận
   // Nhại lại nội dung tin nhắn
-  ChatGPTService.generateCompletion(chatMsg).then(responseMsg => {
-    bot.sendMessage(chatId, responseMsg);
-  });
+  if (chatMsg === "/c") {
+    ChatGPTService.generateCompletion(chatMsg).then((responseMsg) => {
+      bot.sendMessage(chatId, responseMsg);
+    });
+  }
 });
